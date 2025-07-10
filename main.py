b3be18e02c1e18245a801f6271a6dab55024ab3f
aditipatelpro <aditipatelpro@gmail.com>
2025-07-09 22:03:37 -0500

    setup langchain

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
 from typing import Union
 from fastapi import FastAPI
+import getpass
+import os
+from dotenv import load_dotenv
 import yfinance as yf
 
-data = yf.Ticker("MSFT")
+from langchain.chat_models import init_chat_model
+from langchain_core.prompts import ChatPromptTemplate
+
+load_dotenv()
+
+model = init_chat_model("gpt-4o-mini", model_provider="openai")
 
 app = FastAPI()
 
-
 @app.get("/")
 def read_root():
-    return {"ticker": data.info}
+    return {"Welcome to stock-analysis"}
+
+@app.get("/tickers/{ticker}")
+def read_ticker(ticker: str):
+    data = yf.Ticker(ticker)
+
+    system_template = "Provide me a brief valution summary for {value}"
+    prompt_template = ChatPromptTemplate.from_messages([("system", system_template)])
+    prompt = prompt_template.invoke({"value": ticker})
+
+    response = model.invoke(prompt)
+
+    return {"Summary": response.content}
