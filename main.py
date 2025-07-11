241103e3faeb062955edf4babdbf08f59738d140
aditipatelpro <aditipatelpro@gmail.com>
2025-07-10 22:55:53 -0500

    ruff for linting nad formating

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-from typing import Union
 from fastapi import FastAPI
-import getpass
-import os
 from dotenv import load_dotenv
 import yfinance as yf
 
@@ -14,17 +11,24 @@ model = init_chat_model("gpt-4o-mini", model_provider="openai")
 
 app = FastAPI()
 
+
 @app.get("/")
 def read_root():
     return {"Welcome to stock-analysis"}
+
 
 @app.get("/tickers/{ticker}")
 def read_ticker(ticker: str):
     ticker_data = yf.Ticker(ticker)
 
-    prompt_template = ChatPromptTemplate.from_messages([
-        ("system", "Provide me a short stock valuation summary in a single plain text paragraph on the fundamental data from {ticker_info}.")
-    ])
+    prompt_template = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                "Provide me a short stock valuation summary in a single plain text paragraph on the fundamental data from {ticker_info}.",
+            )
+        ]
+    )
     prompt = prompt_template.invoke({"ticker_info": ticker_data.info})
 
     ai_message = model.invoke(prompt)
